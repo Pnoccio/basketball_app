c016d8302d023790c68631418cb2bd7a68ead8fe
Pnoccio <robinshango@gmail.com>
2023-06-08 09:14:35 +0300

    fetching data from the api

--- lib/screens/team_screen.dart
+++ lib/screens/team_screen.dart
@@ -3,89 +3,112 @@ import 'package:basketball_app/utils/app_data.dart';
 import 'package:basketball_app/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
-// import 'package:http/http.dart' as http;
-
 class TeamScreen extends StatefulWidget {
   const TeamScreen({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _TeamScreenState createState() => _TeamScreenState();
 }
 
 class _TeamScreenState extends State<TeamScreen> {
   List<Teams> teamScreen = [];
-  late Future<List> futureTeams;
-  void getTeams() async {
-    futureTeams = AppData.getTeams();
-    setState(() {
-      futureTeams.then((data) {
-        teamScreen = data.map((bkteam) => Teams.teamObj(bkteam)).toList();
-      });
-    });
+  late Future<List<dynamic>> futureTeams;
+
+  Future<List<dynamic>> fetchTeams() async {
+    try {
+      return await AppData.getTeams();
+    } catch (e) {
+      throw Exception('Failed to fetch teams: $e');
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    getTeams();
+    futureTeams = fetchTeams();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-          future: futureTeams,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: teamScreen.length,
-                  itemBuilder: (context, index) {
-                    String teamImages =
-                        'assets/team_logos/team_photo_${index + 1}.png';
-                    return Container(
-                      margin: const EdgeInsets.all(2),
-                      padding: const EdgeInsets.all(2),
-                      decoration: BoxDecoration(
-                        color: Styles.bgColor,
-                        borderRadius: BorderRadius.circular(7),
-                      ),
-                      child: ListTile(
-                        leading: Container(
-                          margin: const EdgeInsets.all(0),
-                          padding: const EdgeInsets.all(10),
-                          child: ClipOval(
-                            child: Image.asset(
-                              teamImages,
-                              height: 50,
-                              width: 50,
-                            ),
-                          ),
-                        ),
-                        title: Text(
-                          teamScreen[index].fullName,
-                          style: Styles.headLineStyle3.copyWith(
-                            color: Colors.black87,
-                          ),
-                        ),
-                        subtitle: Text(
-                          teamScreen[index].abbreviation,
-                          style: Styles.headLineStyle4
-                              .copyWith(color: Colors.black45),
-                        ),
-                        trailing: Text(
-                          teamScreen[index].division,
-                          style: Styles.headLineStyle4,
+      body: FutureBuilder<List<dynamic>>(
+        future: futureTeams,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasData) {
+            teamScreen =
+                snapshot.data!.map((bkteam) => Teams.teamObj(bkteam)).toList();
+            return ListView.builder(
+              itemCount: teamScreen.length,
+              itemBuilder: (context, index) {
+                String teamImages =
+                    'assets/team_logos/team_photo_${index + 1}.png';
+                return Container(
+                  margin: const EdgeInsets.all(2),
+                  padding: const EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    color: Styles.bgColor,
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                  child: ListTile(
+                    leading: Container(
+                      margin: const EdgeInsets.all(0),
+                      padding: const EdgeInsets.all(10),
+                      child: ClipOval(
+                        child: Image.asset(
+                          teamImages,
+                          height: 50,
+                          width: 50,
                         ),
                       ),
-                    );
-                  });
-            } else {
-              return const Center(
-                child: Text("Data is not found"),
-              );
-            }
-          }),
+                    ),
+                    title: Text(
+                      teamScreen[index].fullName,
+                      style: Styles.headLineStyle3.copyWith(
+                        color: Colors.black87,
+                      ),
+                    ),
+                    subtitle: Text(
+                      teamScreen[index].abbreviation,
+                      style:
+                          Styles.headLineStyle4.copyWith(color: Colors.black45),
+                    ),
+                    trailing: Text(
+                      teamScreen[index].division,
+                      style: Styles.headLineStyle4,
+                    ),
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Error: ${snapshot.error}',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        futureTeams = fetchTeams();
+                      });
+                    },
+                    child: Text('Retry'),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return const Text('No data found');
+          }
+        },
+      ),
     );
   }
 }
--- lib/utils/app_data.dart
+++ lib/utils/app_data.dart
@@ -1,34 +1,72 @@
 import 'dart:convert';
-
+import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart';
 
 class AppData {
-  static String playersJson = 'assets/json/players.json';
-  static String gamesJson = 'assets/json/games.json';
-  static String teamsJson = 'assets/json/teams.json';
-  static String statsJson = 'assets/json/stata.json';
-
-  static Future<List> getPlayers() async {
-    final String response = await rootBundle.loadString(playersJson);
-    final List data = await jsonDecode(response)['data'];
-    return data;
+  static Future<List<dynamic>> getPlayers() async {
+    final response =
+        await http.get(Uri.parse('https://www.balldontlie.io/api/v1/players'));
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body)['data'];
+      return data;
+    } else {
+      throw Exception('Failed to fetch players');
+    }
   }
 
-  static Future<List> getTeams() async {
-    final String response = await rootBundle.loadString(teamsJson);
-    final List data = await jsonDecode(response)['data'];
-    return data;
+  static Future<List<dynamic>> getTeams() async {
+    final response =
+        await http.get(Uri.parse('https://www.balldontlie.io/api/v1/teams'));
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body)['data'];
+      return data;
+    } else {
+      throw Exception('Failed to fetch teams');
+    }
   }
 
-  static Future<List> getGames() async {
-    final String response = await rootBundle.loadString(gamesJson);
-    final List data = await jsonDecode(response)['data'];
-    return data;
-  }
-
-  static Future<List> getStats() async {
-    final String response = await rootBundle.loadString(statsJson);
-    final List data = await jsonDecode(response)['data'];
-    return data;
+  static Future<List<dynamic>> getGames() async {
+    final response =
+        await http.get(Uri.parse('https://www.balldontlie.io/api/v1/games'));
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body)['data'];
+      return data;
+    } else {
+      throw Exception('Failed to fetch games');
+    }
   }
 }
+
+  // static String playersJson = 'assets/json/players.json';
+  // static String gamesJson = 'assets/json/games.json';
+  // static String teamsJson = 'assets/json/teams.json';
+  // static String statsJson = 'assets/json/stata.json';
+
+  // static Future<List> getPlayers() async {
+  //   final String response = await rootBundle.loadString(playersJson);
+  //   final List data = await jsonDecode(response)['data'];
+  //   return data;
+  // }
+
+  // static Future<List> getTeams() async {
+  //   final String response = await rootBundle.loadString(teamsJson);
+  //   final List data = await jsonDecode(response)['data'];
+  //   return data;
+  // }
+
+  // static Future<List> getGames() async {
+  //   final String response = await rootBundle.loadString(gamesJson);
+  //   final List data = await jsonDecode(response)['data'];
+  //   return data;
+  // }
+
+  // static Future<List> getStats() async {
+  //   final String response = await rootBundle.loadString(statsJson);
+  //   final List data = await jsonDecode(response)['data'];
+  //   return data;
+  // }
+//}
+
+
+
+
